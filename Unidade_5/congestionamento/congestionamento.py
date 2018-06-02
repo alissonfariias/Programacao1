c872f18543ceb887097b8c5933fc8e58df3b0cd3
alissonfariias <walisson.farias@ccc.ufcg.edu.br>
2018-06-01 23:27:08 -0300

    Up de programas da Unidade 5

---
+++ Unidade_5/congestionamento/congestionamento.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+lista = []
+cont = 0
+while True:
+	entrada = raw_input()
+	for letra in entrada:
+		if letra == 'v':
+			cont += 1
+		elif letra != 'v':
+			lista.append(cont)
+			cont = 0
+	lista.append(cont)
+	maior = 0
+	for num in lista:
+		if num > maior:
+			maior = num
+	print maior
+	lista = []
+	break
---
+++ Unidade_5/digito_por_digito/digito_por_digito.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+numero = int(raw_input())
+while numero != 0:
+	print numero % 10
+	numero /= 10
